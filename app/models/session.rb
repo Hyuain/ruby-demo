601e8652753cc9bc063acae44a6609e5ca5ee375
Hyuain <Hyuain97@163.com>
2021-02-17 09:19:37 +0800

    feat: 校验密码是否匹配

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,18 +1,26 @@
 class Session
   include ActiveModel::Model
-  attr_accessor :email, :password
+  attr_accessor :email, :password, :user
 
   validates :email, presence: true
-  validate :check_email, if: :email
   validates :password, presence: true
 
   validates_format_of :email, with: /.+@.+/, if: :email
+  validate :check_email, if: :email
   validates_length_of :password, minimum: 6, if: :password
+  validate :check_email_password_matched, if: Proc.new{ |s| s.email.present? and s.password.present? }
 
   def check_email
-    user = User.find_by email: email
+    user ||= User.find_by email: email
     if user.nil?
       errors.add :email, :unregistered
+    end
+  end
+
+  def check_email_password_matched
+    user ||= User.find_by email: email
+    if user and !user.authenticate(password)
+      errors.add :password, :mismatch
     end
   end
 end
