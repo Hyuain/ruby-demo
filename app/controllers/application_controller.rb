9f03a4c9e985120acd1db98b2d56f85b59b94914
Hyuain <Hyuain97@163.com>
2021-05-19 21:29:29 +0800

    登录检查，如果没有登录，就返回 401

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
+require 'custom_error'
+
 class ApplicationController < ActionController::API
+  rescue_from CustomError::MustSignInError, with: :render_must_sign_in
+
+  def must_sign_in
+    if current_user.nil?
+      raise CustomError::MustSignInError
+    end
+  end
+
   def current_user
     # 如果找不到，会返回 nil，ruby 这样会默认返回 current_user
     @current_user ||= User.find_by_id session[:current_user_id]
@@ -13,5 +23,9 @@ class ApplicationController < ActionController::API
     else
       render json: { errors: resource.errors }, status: 422
     end
+  end
+
+  def render_must_sign_in
+    render status: :unauthorized
   end
 end
--- spec/requests/records_request_spec.rb
+++ spec/requests/records_request_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Record', type: :request do
+  it '未登录不能创建 record' do
+    post '/records', params: { amount: 10000, category: 'outgoings', notes: '吃饭' }
+    expect(response.status).to eq 401
+  end
   it '创建一个 record' do
     post '/records', params: { amount: 10000, category: 'outgoings', notes: '吃饭' }
     expect(response.status).to eq 200
