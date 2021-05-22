7acfb760cc7b29e6cd285905f1c3194115ac19c5
Hyuain <Hyuain97@163.com>
2021-05-22 17:26:47 +0800

    测试 tag 和 model 的关联

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,6 @@
 class Tag < ApplicationRecord
   has_many :taggings
+  has_many :records, through: :taggings
 
   validates_presence_of :name
 end
--- spec/models/tagging_spec.rb
+++ spec/models/tagging_spec.rb
@@ -13,4 +13,25 @@ RSpec.describe Tagging, type: :model do
     expect(tagging.errors.details[:tag][0][:error]).to eq(:blank)
     expect(tagging.errors[:tag][0]).to eq('标签不能为空')
   end
+  it '可以创建关联' do
+    tag = Tag.create name: 'test'
+    record = Record.create amount: 10000, category: 'income'
+    Tagging.create record: record, tag: tag
+    expect(tag.records.first.id).to eq record.id
+    expect(record.tags.first.id).to eq tag.id
+  end
+  it '可以多对多关联' do
+    tag1 = Tag.create name: 'test1'
+    tag2 = Tag.create name: 'test2'
+    record1 = Record.create amount: 10000, category: 'income'
+    record2 = Record.create amount: 10000, category: 'income'
+    Tagging.create record: record1, tag: tag1
+    Tagging.create record: record1, tag: tag2
+    Tagging.create record: record2, tag: tag1
+    Tagging.create record: record2, tag: tag2
+    expect(tag1.records.count).to eq 2
+    expect(tag2.records.count).to eq 2
+    expect(record1.tags.count).to eq 2
+    expect(record2.tags.count).to eq 2
+  end
 end
