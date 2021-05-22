534cb9b2aa65a64455fcd425dcc9a0b6233ccad7
Hyuain <Hyuain97@163.com>
2021-05-22 17:21:03 +0800

    测试 tagging model

--- spec/models/tagging_spec.rb
+++ spec/models/tagging_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Tagging, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it '必须传 record' do
+    tag = Tag.create name: 'test'
+    tagging = Tagging.create tag: tag
+    expect(tagging.errors.details[:record][0][:error]).to eq(:blank)
+    expect(tagging.errors[:record][0]).to eq('记录不能为空')
+  end
+  it '必须传 tag' do
+    record = Record.create amount: 10000, category: 'income'
+    tagging = Tagging.create record: record
+    expect(tagging.errors.details[:tag][0][:error]).to eq(:blank)
+    expect(tagging.errors[:tag][0]).to eq('标签不能为空')
+  end
 end
